a8dd28fc0a957f353e3aa344c673004a3129d9ea
MahajanPyanshu <tarzan144144@gmail.com>
2026-06-03 23:20:03 +0200

    rankaa

--- src/Main2.java
+++ src/Main2.java
@@ -2,7 +2,7 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("ok");
         for(int i=1;i<=40;i++){
-            System.out.println(i);
+            System.out.println(i+"rankaa");
         }
     }
 }
